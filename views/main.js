8bde968f2c9541ddf0f31eff41a9a61779036d95
tomasmonge2704 <tomas.monge.vidal@gmail.com>
2022-07-15 21:37:04 -0300

    added buscar y filtrar

--- views/main.js
+++ views/main.js
@@ -1,57 +1,67 @@
-function deleteProd(id){
-axios.delete(`http://localhost:8080/api/productos/${id}`)
-.then((response) => {
-    location.reload();
-    console.log(response)
-})
-.catch((err)=>{
-    console.log(err)
-})
+function deleteProd(id) {
+  axios
+    .delete(`http://localhost:8080/api/productos/${id}`)
+    .then((response) => {
+      location.reload();
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
-function addProd(elem){
+function addProd(elem) {
+  var nombre = document.getElementById("nombre").value;
+  var categoria = document.getElementById("categoria").value;
+  var precio = document.getElementById("precio").value;
+  var fotoURL = document.getElementById("fotoURL").value;
+  var codigo = document.getElementById("codigo").value;
+  var stock = document.getElementById("stock").value;
 
-var nombre = document.getElementById('nombre').value
-var categoria = document.getElementById('categoria').value
-var precio = document.getElementById('precio').value
-var fotoURL = document.getElementById('fotoURL').value
-var codigo = document.getElementById('codigo').value
-var stock = document.getElementById('stock').value
-
-axios.post(`http://localhost:8080/api/productos/`,{
-  nombre:nombre,
-  categoria:categoria,
-  precio:precio,
-  fotoURL:fotoURL,
-  codigo:codigo,
-  stock:stock
-})
-.then((response) => {
-    console.log(response)
-    location.reload();
-})
-.catch((err)=>{
-    console.log(err)
-})
+  axios
+    .post(`http://localhost:8080/api/productos/`, {
+      nombre: nombre,
+      categoria: categoria,
+      precio: precio,
+      fotoURL: fotoURL,
+      codigo: codigo,
+      stock: stock,
+    })
+    .then((response) => {
+      console.log(response);
+      location.reload();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 function addToCart(id) {
-
-axios.get(`http://localhost:8080/api/productos/${id}`)
-.then((response) => {
-        const elem = response.data[0]
-        axios.post(`http://localhost:8080/api/carrito/${user}/productos`,{
-  id:elem.id,
-  nombre:elem.nombre,
-  precio:elem.precio,
-  fotoURL:elem.fotoURL,
-  codigo:elem.codigo,
-  stock:elem.stock
-})
-      .then((response) => {
-        console.log(response)
+  axios
+    .get(`http://localhost:8080/api/productos/${id}`)
+    .then((response) => {
+      const elem = response.data[0];
+      axios
+        .post(`http://localhost:8080/api/carrito/${user}/productos`, {
+          id: elem.id,
+          nombre: elem.nombre,
+          precio: elem.precio,
+          fotoURL: elem.fotoURL,
+          codigo: elem.codigo,
+          stock: elem.stock,
+        })
+        .then((response) => {
+          console.log(response);
+        });
     })
-    })
-.catch((err) => {
-        console.log(err)
+    .catch((err) => {
+      console.log(err);
     });
 }
 
+function filtrarCategoria() {
+  var categoria = document.getElementById("exampleFormControlSelect1").value;
+  window.location.href = `/productos/${categoria}`;
+}
+function filtrarId() {
+  var id = document.getElementById("exampleFormControlSelect2").value;
+  window.location.href = `/producto/${id}`;
+}
